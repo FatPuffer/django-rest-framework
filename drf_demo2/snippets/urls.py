da4e6aa89d0de257dfe59b0961c16ec07e996862
fatpuffer <fatpuffer@163.com>
2019-04-10 21:59:21 +0800

    hyperlink

--- drf_demo2/snippets/urls.py
+++ drf_demo2/snippets/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>\d+)$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view())
-]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns([
+    url(r'^$', views.api_root),    
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>\d+)$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^snippets/(?P<pk>\d+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view(), name='user-detail'),
+])
 
